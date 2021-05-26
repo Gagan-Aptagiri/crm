07c527494007330324b16eb5d947c78b12f4be5b
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-26 10:57:07 +0530

    CustomerDAO interface added

---
+++ Web-Customer-Tracker/src/com/gagan/aptagiri/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package com.gagan.aptagiri.dao;
+
+import java.util.List;
+
+import com.gagan.aptagiri.entity.Customer;
+
+public interface CustomerDAO {
+	
+	public List<Customer> getCustomers();
+}
