91f2d1ab2b1e9ae19f062dfbc2838285278feb1c
Gagan-Aptagiri <gagan.aptagiri@gmail.com>
2021-05-26 12:38:34 +0530

    Added a customer service layer which takes in customerDAO

---
+++ Web-Customer-Tracker/src/com/gagan/aptagiri/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.gagan.aptagiri.service;
+
+import java.util.List;
+
+import com.gagan.aptagiri.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+}
